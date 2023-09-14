bb497f21f02258e76255e4924f0c79b69f73cbc6
JakeHowdeshell <135374279+JakeHowdeshell@users.noreply.github.com>
2023-09-13 22:30:44 -0500

    removed the async from the put route

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -33,7 +33,7 @@ router.get("/:id", async (req, res) => {
 });
 
 // creating new product
-router.post("/", async (req, res) => {
+router.post("/", (req, res) => {
   Product.create(req.body)
     .then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
